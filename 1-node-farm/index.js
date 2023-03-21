7ef8fd2318f5abce46f5655cf90245abcf7a5818
mdemon420 <mdeamon4@gmail.com>
2023-03-21 21:12:56 +0600

    replace template cart details

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -38,23 +38,52 @@ const url = require('node:url');
 // Server           /
 ////////////////////
 
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%PRODUCTNAME%}/g, product.name);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+};
+
+const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+const templateCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+
+
+
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
+    // OVERVIEW PAGE
     if (pathName === '/' || pathName === '/overview') {
-        res.end('This is the overview');
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        const cartHtml = dataObj.map(el => replaceTemplate(templateCard, el));
+        const output = templateOverview.replace('{%PRODUCT_CARDS%}', cartHtml);
+        console.log(cartHtml);
+        res.end(output);
     }
+    //PRODUCT PAGE
     else if (pathName === '/product') {
         res.end('This is the product');
     }
+    // API
     else if (pathName === '/api') {
         res.writeHead(200, { 'Content-Type': 'application/json' })
         res.end(data)
         console.log(dataObj);
     }
+    // NOT FOUND
     else {
         res.writeHead(404, {
             'Content-type': 'text/html',

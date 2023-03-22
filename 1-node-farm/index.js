112fba00ec525b7518448272d7867bfa654825b4
mdemon420 <mdeamon4@gmail.com>
2023-03-22 13:06:11 +0600

    event loop

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -2,6 +2,8 @@ const fs = require('node:fs');
 const http = require('node:http');
 const url = require('node:url');
 
+const replaceTemplate = require('./module/replaceTemplate');
+
 //////////////////////
 //  Files           /
 ////////////////////
@@ -38,19 +40,7 @@ const url = require('node:url');
 // Server           /
 ////////////////////
 
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.name);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
 
-    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-};
 
 const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
@@ -63,22 +53,25 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
+    const { query, pathname } = url.parse(req.url, true);
 
     // OVERVIEW PAGE
-    if (pathName === '/' || pathName === '/overview') {
+    if (pathname === '/' || pathname === '/overview') {
         res.writeHead(200, { 'Content-Type': 'text/html' });
-        const cartHtml = dataObj.map(el => replaceTemplate(templateCard, el));
+        const cartHtml = dataObj.map(el => replaceTemplate(templateCard, el)).join('');
         const output = templateOverview.replace('{%PRODUCT_CARDS%}', cartHtml);
-        console.log(cartHtml);
         res.end(output);
     }
     //PRODUCT PAGE
-    else if (pathName === '/product') {
-        res.end('This is the product');
+    else if (pathname === '/product') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        const product = dataObj[query.id];
+        const output = replaceTemplate(templateProduct, product)
+
+        res.end(output);
     }
     // API
-    else if (pathName === '/api') {
+    else if (pathname === '/api') {
         res.writeHead(200, { 'Content-Type': 'application/json' })
         res.end(data)
         console.log(dataObj);
@@ -96,5 +89,3 @@ const server = http.createServer((req, res) => {
 server.listen(8000, () => {
     console.log('Listening to request on port 8000');
 });
-
-

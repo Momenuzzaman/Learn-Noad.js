5915570e9d3b4e037e502a506105ca0bb6ccff1e
mdemon420 <mdeamon4@gmail.com>
2023-04-06 14:11:48 +0600

    post method

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -3,6 +3,8 @@ const fs = require('fs');
 const app = express();
 const port = 5000;
 
+app.use(express.json());
+
 // app.get('/', (req, res) => {
 //     res.json({ app: 'Hello form the server side ok!' });
 // });
@@ -12,9 +14,11 @@ const port = 5000;
 //     res.send('You can post to this endpoint.....');
 // })
 
+
 const tours = JSON.parse(
     fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 )
+// get
 app.get('/api/v1/tours', (req, res) => {
     res.status(200).json({
         status: 'success',
@@ -24,6 +28,20 @@ app.get('/api/v1/tours', (req, res) => {
         }
     })
 })
+// post
+app.post('/api/v1/tours', (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
+    });
+});
 console.log(tours)
 app.listen(port, () => {
     console.log(`App running on port ${port}`);

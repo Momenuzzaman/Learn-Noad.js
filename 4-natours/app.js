d7d584849ae1b461f294b94f1b310cea9b382273
mdemon420 <mdeamon4@gmail.com>
2023-04-06 13:16:17 +0600

    red json file

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -1,17 +1,30 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
-    res.json({ app: 'Hello form the server side ok!' });
-});
+// app.get('/', (req, res) => {
+//     res.json({ app: 'Hello form the server side ok!' });
+// });
 
 
-app.post('/', (req, res) => {
-    res.send('You can post to this endpoint.....');
+// app.post('/', (req, res) => {
+//     res.send('You can post to this endpoint.....');
+// })
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+)
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        result: tours.length,
+        data: {
+            tours: tours
+        }
+    })
 })
-
+console.log(tours)
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
 });
